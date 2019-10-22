38ee307a20b8ae297109aeee232397ebeec6544e
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-10-22 16:46:34 +0000

    change names

--- app.py
+++ app.py
@@ -15,24 +15,27 @@ def homepage():
 
 @app.route('/get_id')
 def get_id():
-    try:
-        with conn.cursor() as cursor:
-            conn.autocommit = True
-            sql_get = "SELECT ID FROM cluster WHERE STATE=1"
-            cursor.execute(sql_get)
-            count = cursor.fetchone()
-            sql_update = "UPDATE cluster SET state=2 WHERE ID={}".format(count[0])
-            cursor.execute(sql_update)
-            return jsonify(id=count[0])
-    except Exception as ex:
-        print(ex)
+   try:
+       with conn.cursor() as cursor:
+           conn.autocommit = True
+
+           sql_select = "SELECT TASK_ID FROM state_table WHERE STATE=1"
+           cursor.execute(sql_select)
+           task_id= cursor.fetchone()
+
+           sql_update = "UPDATE state_table SET state=2 WHERE TASK_ID={}".format(task_id[0])
+           cursor.execute(sql_update)
+           return jsonify(id=task_id[0])
+   except Exception as ex:
+       print(ex)
+
 
 @app.route('/close_id/<id>')
 def close_id(id):
     try:
         with conn.cursor() as cursor:
             conn.autocommit = True
-            sql_update = "UPDATE cluster SET state=3 WHERE ID={}".format(id)
+            sql_update = "UPDATE state_table SET state=3 WHERE TASK_ID={}".format(id)
             cursor.execute(sql_update)
             return 'ok'
     except Exception as ex:
@@ -43,17 +46,19 @@ def close_id(id):
 @app.route('/clean')
 def clean():
     try:
+
         with conn.cursor() as cursor:
             conn.autocommit = True
 
-            clean = sql.SQL('TRUNCATE table cluster')
+            clean = sql.SQL('TRUNCATE table state_table')
             cursor.execute(clean)
+
         list_data=[]
         for i in range(1, 40000):
             list_data.append((i,1))
         with conn.cursor() as cursor:
             conn.autocommit = True
-            insert = sql.SQL('INSERT INTO cluster (id,state) VALUES {}').format(
+            insert = sql.SQL('INSERT INTO state_table (task_id,state) VALUES {}').format(
                 sql.SQL(',').join(map(sql.Literal, list_data))
             )
 
