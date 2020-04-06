cedd3f156038dc81d97abf849ece68155a594864
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2020-04-06 04:00:21 +0300

    api users minecraft

--- app.py
+++ app.py
@@ -1,73 +1,207 @@
-from flask import Flask, jsonify
-import psycopg2
-from psycopg2 import sql
+import json
+import threading
+import os
+from flask import Flask, jsonify, g
+import sqlite3
 
-
+try:
+    from ssh_client import send
+except:
+    print('no paramilo')
 app = Flask(__name__)
 
-conn = psycopg2.connect(dbname='mydb', user='myuser',
-                        password='12345678', host='myhost.com')
+file_name='minecraft.db'
+path = os.path.dirname(os.path.abspath(__file__))+'/'
+
+LIFE=10
+KILL=5
+WIN=50
+
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(path+"minecraft.db")
+
+    return db
+
+@app.teardown_appcontext
+def close_cursorection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def create_db():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE users (name TEXT , password TEXT, money INTEGER, life INTEGER, wins INTEGER ,kills INTEGER, dies INTEGER)")
+    cursor.execute("INSERT INTO users VALUES ('1234', 'eee', 0,0,0,0,0)")
+    db.commit()
+
+def save_to_file():
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM users "
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    str_data = json.dumps(data)
+    print(str_data)
+    with open('users.txt', 'w') as file:
+        file.write(str_data)
+
+
 
 @app.route('/')
 def homepage():
-    return "hello"
+    try:
+        return "hello"
+    except Exception as ex:
+        print(ex)
+        return 'ER'
 
+@app.route('/all')
+def homepage2():
+    sql = "SELECT * FROM users "
+    cursor=get_db().cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    str_data = json.dumps(data)
+    print(str_data)
+    return str_data
 
-@app.route('/get_id')
-def get_id():
+@app.route('/kill/<name>')
+def add_kill(name):
    try:
-       with conn.cursor() as cursor:
-           conn.autocommit = True
-
-           sql_select = "SELECT TASK_ID FROM state_table WHERE STATE=1"
-           cursor.execute(sql_select)
-           task_id= cursor.fetchone()
-
-           sql_update = "UPDATE state_table SET state=2 WHERE TASK_ID={}".format(task_id[0])
-           cursor.execute(sql_update)
-           return jsonify(id=task_id[0])
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}'".format(name)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       sql_money = "UPDATE users SET money = {} WHERE name ='{}'".format(data[2] + KILL, name)
+       cursor.execute(sql_money)
+       db.commit()
+       sql = "UPDATE users SET kills = {} WHERE name ='{}'".format(data[5]+1, name)
+       cursor.execute(sql)
+       db.commit()
+       return 'OK'
    except Exception as ex:
        print(ex)
+       return 'ER'
 
+@app.route('/check/<name>')
+def check(name):
+   try:
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}'".format(name)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       if data[2]>=LIFE:
+         return 'OK'
+       else:
+         return 'ER'
+   except Exception as ex:
+       print(ex)
+       return 'ER'
 
-@app.route('/close_id/<id>')
-def close_id(id):
+@app.route('/win/<name>')
+def win(name):
+   try:
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}'".format(name)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       sql_money = "UPDATE users SET money = {} WHERE name ='{}'".format(data[2] + WIN, name)
+       cursor.execute(sql_money)
+       db.commit()
+       sql = "UPDATE users SET wins = {} WHERE name ='{}'".format(data[4]+1, name)
+       cursor.execute(sql)
+       db.commit()
+       return 'OK'
+   except Exception as ex:
+       print(ex)
+       return 'ER'
+
+@app.route('/die/<name>')
+def die(name):
+   try:
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}'".format(name)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       sql_money = "UPDATE users SET money = {} WHERE name ='{}'".format(data[2] - LIFE, name)
+       cursor.execute(sql_money)
+       db.commit()
+       sql_money = "UPDATE users SET dies = {} WHERE name ='{}'".format(data[6] + 1, name)
+       cursor.execute(sql_money)
+       db.commit()
+       return 'OK'
+   except Exception as ex:
+       print(ex)
+       return 'ER'
+
+@app.route('/getinfo/<name>')
+def getinfo(name):
+   try:
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}'".format(name)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       if data is None:
+           return 'ER'
+       return jsonify(data)
+   except Exception as ex:
+       print(ex)
+       return 'ER'
+
+@app.route('/login/<name>&<password>')
+def login(name,password):
+   try:
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}' and password='{}'".format(name,password)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       if data is None:
+           return 'ER'
+       return jsonify(data)
+   except Exception as ex:
+       print(ex)
+       return 'ER'
+
+@app.route('/reg/<name>&<password>')
+def reg(name,password):
+   try:
+       db = get_db()
+       cursor = db.cursor()
+       sql_user = "SELECT * FROM users where name='{}'".format(name)
+       cursor.execute(sql_user)
+       data= cursor.fetchone()
+       if data is None:
+           cursor = db.cursor()
+           cursor.execute("INSERT INTO users VALUES ('{}', '{}', 0,0,0,0,0)".format(name,password))
+           db.commit()
+           return 'OK'
+       return 'ER'
+   except Exception as ex:
+       print(ex)
+       return 'ER'
+
+def timer_start():
+    threading.Timer(120.0, timer_start).start()
+    print('start')
     try:
-        with conn.cursor() as cursor:
-            conn.autocommit = True
-            sql_update = "UPDATE state_table SET state=3 WHERE TASK_ID={}".format(id)
-            cursor.execute(sql_update)
-            return 'ok'
+        send()
     except Exception as ex:
         print(ex)
-
-
-
-@app.route('/clean')
-def clean():
-    try:
-
-        with conn.cursor() as cursor:
-            conn.autocommit = True
-
-            clean = sql.SQL('TRUNCATE table state_table')
-            cursor.execute(clean)
-
-        list_data=[]
-        for i in range(1, 40000):
-            list_data.append((i,1))
-        with conn.cursor() as cursor:
-            conn.autocommit = True
-            insert = sql.SQL('INSERT INTO state_table (task_id,state) VALUES {}').format(
-                sql.SQL(',').join(map(sql.Literal, list_data))
-            )
-
-            cursor.execute(insert)
-    except Exception as ex:
-        print(ex)
-    return 'ok'
 
 
 if __name__ == '__main__':
+
+    timer_start()
     app.run()
 
---
+++ ssh_client.py
@@ -0,0 +1,31 @@
+import paramiko
+import os
+
+host = 'ec2-13-48-1-143.eu-north-1.compute.amazonaws.com'
+user = 'new_user'
+secret = '123456'
+port = 22
+file_name='minecraft.db'
+path = os.path.dirname(os.path.abspath(__file__))+'/'
+
+def send():
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=secret, port=port)
+    ftp_client = client.open_sftp()
+    ftp_client.put(path+file_name,file_name)
+    ftp_client.close()
+    client.close()
+
+def get():
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=secret, port=port)
+    ftp_client = client.open_sftp()
+
+    ftp_client.get(file_name,path+file_name)
+    ftp_client.close()
+    client.close()
+
+if __name__ == '__main__':
+    get()
